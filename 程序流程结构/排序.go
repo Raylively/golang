ef3bbb0a8f8abf23d6af8c878854ddbf625b71b7
Raylively <40203616+Raylively@users.noreply.github.com>
2020-04-30 21:42:01 +0800

    Update 排序.go

--- 程序流程结构/排序.go
+++ 程序流程结构/排序.go
@@ -1,20 +1,13 @@
-package main 
-
-import (
-    "fmt"
-)
-
-
 func main(){
 	/*
 	冒泡排序
 	*/
 
 	arr1 := [10]int{22,67,32,45,98,75,19,54,39,11}
-	for j:= cap(arr1)-1; j>0;j--{
-		for i:=0; i<cap(arr1)-1; i++{
-			if arr1[i] > arr1[i+1]{
-				arr1[i],arr1[i+1] = arr1[i+1],arr1[i]
+	for i:= 1; i<len(arr1);i++{
+		for j:=0; j<cap(arr1)-i; j++{
+			if arr1[j] > arr1[j+1]{
+				arr1[j],arr1[j+1] = arr1[j+1],arr1[j]
 			}
 		}
 	}
